04fed6f1322e4581ff828df22d9965fb9eec5b77
l0gik67 <l0gik67@mail.ru>
2026-02-18 21:50:37 +0300

    Solve Task7

--- src/main/java/tasks/Task7.java
+++ src/main/java/tasks/Task7.java
@@ -1,9 +1,12 @@
 package tasks;
 
 import common.Company;
+import common.Vacancy;
+
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /*
 Из коллекции компаний необходимо получить всевозможные различные названия вакансий
@@ -11,7 +14,16 @@ import java.util.Set;
 public class Task7 {
 
   public static Set<String> vacancyNames(Collection<Company> companies) {
-    return new HashSet<>();
+    return companies.stream()
+            .map(Company::getVacancies)
+            .flatMap(Set::stream)
+            .map(Vacancy::getTitle)
+            .collect(Collectors.toSet());
+    // если рассматривать n - кол-во компаний m - максимальное число различных вакансий у каждой
+    // по памяти получим O(nm) - (это будет до того как
+    // преобразовали stream в set), после преобразования в сет, будет O(k) - k количество уникальных вакансий
+    // логичнее взять максимальную границу O(nm)
+    // по времени O(nm) - каждую вакансию просмотрим один раз
   }
 
 }
