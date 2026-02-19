1f98c17817ad9aeed3ca949d7bad307a79411e65
l0gik67 <l0gik67@mail.ru>
2026-02-19 12:54:06 +0300

    Solve Task6

--- src/main/java/tasks/Task6.java
+++ src/main/java/tasks/Task6.java
@@ -2,10 +2,8 @@ package tasks;
 
 import common.Area;
 import common.Person;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+
+import java.util.*;
 
 /*
 Имеются
@@ -16,9 +14,36 @@ import java.util.Set;
  */
 public class Task6 {
 
+  public Task6() {
+  }
+
   public static Set<String> getPersonDescriptions(Collection<Person> persons,
                                                   Map<Integer, Set<Integer>> personAreaIds,
                                                   Collection<Area> areas) {
-    return new HashSet<>();
+    Map<Integer, Person> mapPersons  = new HashMap<>();
+    for (Person person : persons) {
+      mapPersons.put(person.id(), person); // добавляю в мапу всех person по id, чтобы потом можно было получить их
+      // за О(1), не использую поток, чтобы не тратить лишнюю память
+    }
+
+    // то же самое нужно сделать для регионов
+    Map<Integer, Area> mapAreas = new HashMap<>();
+    for (Area area : areas) {
+      mapAreas.put(area.getId(), area);
+    }
+    // по месту пока получаем O(n+m) - количество персон + количество регионов
+
+    Set<String> result = new HashSet<>();
+    for (Map.Entry<Integer, Set<Integer>> entry : personAreaIds.entrySet()) {
+      String personName = mapPersons.get(entry.getKey()).firstName();
+      for (Integer areaId : entry.getValue()) {
+        String areaName = mapAreas.get(areaId).getName();
+        result.add(personName + " - " + areaName);
+      }
+    } // заполняем set через две мапы готовы сочетаниями строк
+    // по месту выйдет O(nm) если у каждого человека уникальное имя и присутсвуют все вакансии из списка
+    // по времени получим также в худшем случае O(nm) // если у каждого есть все m вакансий
+
+    return result;
   }
 }
