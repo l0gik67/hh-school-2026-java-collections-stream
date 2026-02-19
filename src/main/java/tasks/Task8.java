635a22de43ec224504426fef1b12a13ac7fe6844
l0gik67 <l0gik67@mail.ru>
2026-02-19 13:35:53 +0300

    Solve Task8

--- src/main/java/tasks/Task8.java
+++ src/main/java/tasks/Task8.java
@@ -4,8 +4,9 @@ import common.Person;
 import common.PersonService;
 import common.PersonWithResumes;
 import common.Resume;
-import java.util.Collection;
-import java.util.Set;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 /*
   Еще один вариант задачи обогащения
@@ -21,7 +22,29 @@ public class Task8 {
   }
 
   public Set<PersonWithResumes> enrichPersonsWithResumes(Collection<Person> persons) {
-    Set<Resume> resumes = personService.findResumes(Set.of());
-    return Set.of();
+    Set<Integer> personIds = new HashSet<>(); // создаю set id для того что получить по нему резюме пользователей
+    // не использую stream так как просто создаю set место O(n) n - количество пользователей, время также
+    for (Person person : persons) {
+      personIds.add(person.id());
+    }
+    Set<Resume> resumes = personService.findResumes(personIds);
+
+    // создаю через стрим группировкой коллекцию id пользовтелей и их резюме основываясь на поле personId в Resume
+    // группировка должна пройти за O(m) по времени и месту, где m - количество резюме
+    Map<Integer, Set<Resume>> mapIdPersonAndResume = resumes.stream()
+            .collect(Collectors.groupingBy(Resume::personId, Collectors.toSet()));
+
+
+    Set<PersonWithResumes> result = new HashSet<>();
+    // теперь по каждому пользователю из переданной коллекции находим его множество резюме и создаем элемент
+    // PersonWithResumes и месту O(n+m)
+    // по времени O(n)
+
+    for (Person person : persons) {
+      result.add(new PersonWithResumes(person, mapIdPersonAndResume.getOrDefault(person.id(),  Collections.emptySet())));
+    }
+
+    // Общее время алгоритма O(max(n, m)) по месту O(n+m)
+  return result;
   }
 }
